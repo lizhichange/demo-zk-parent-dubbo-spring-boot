53d7fece1ba904d56b00927719f25cda5f595f73
lizhichange@163.com <lizhi1234>
2019-09-05 14:06:40 +0800

    f

--- demo-zk-service/src/main/java/com/fulihui/demo/zk/ServiceApplication.java
+++ demo-zk-service/src/main/java/com/fulihui/demo/zk/ServiceApplication.java
@@ -8,16 +8,18 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-/** @author lizhi */
+/**
+ * @author lizhi
+ */
 @SpringBootApplication
 @EnableScheduling
-@ComponentScan(basePackages = {"com.fulihui.prizecore"})
+@ComponentScan(basePackages = {"com.fulihui"})
 @MapperScan(basePackages = {"com.fulihui.prizecore.dal.dao"})
 @EnableTransactionManagement
 public class ServiceApplication {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    new SpringApplicationBuilder(ServiceApplication.class).web(WebApplicationType.NONE).run(args);
-  }
+        new SpringApplicationBuilder(ServiceApplication.class).web(WebApplicationType.NONE).run(args);
+    }
 }
