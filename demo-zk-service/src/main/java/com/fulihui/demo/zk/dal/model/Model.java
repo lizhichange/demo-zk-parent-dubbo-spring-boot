667b9ca68478de896be28c34024b8434448c7f36
Your Name <lizhichange@163.com>
2020-08-30 17:45:17 +0800

    1

---
+++ demo-zk-service/src/main/java/com/fulihui/demo/zk/dal/model/Model.java
@@ -0,0 +1,20 @@
+package com.fulihui.demo.zk.dal.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author sunflower
+ */
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Model {
+
+    private Integer id;
+
+}
